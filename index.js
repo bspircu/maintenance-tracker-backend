29df216ddd5c115ba7d44d49b90f52e867a6a87d
Bryan Spircu <bspircu@gmail.com>
2019-11-03 17:32:16 -0800

    newest updates

--- index.js
+++ index.js
@@ -3,23 +3,36 @@ const knexConfig = require("./knexfile");
 const { Model } = require("objection");
 const express = require("express");
 const User = require("./models/User");
+const cors = require("cors");
+var bodyParser = require("body-parser");
 
 const dataBase = knex(knexConfig.development);
 Model.knex(dataBase);
 
 const app = express();
 const port = 3000;
+app.use(cors());
+app.options("*", cors());
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/users", async (req, res) => {
   const users = await User.query();
   res.send(JSON.stringify(users));
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.post("/user", async (req, res) => {
+  console.log(req.body);
   const newUser = await User.query().insert({
-    firstName: "Brian",
-    lastName: "Hall"
+    firstName: req.body.firstName,
+    lastName: req.body.lastName
   });
-  console.log(newUser);
+  // console.log(req.body);
+  // const newUser = await User.query().insert({
+  //   firstName: "Brian",
+  //   lastName: "Hall"
+  // });
   res.sendStatus(200);
 });
 app.get("/users/:userId", async (req, res) => {
